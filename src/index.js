99d02305f4b1131675c897e03fda5090ee1e035c
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-11-15 13:07:28 +0800

    Refactor index.js; Change div to button

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import { renderHomePage } from "./homePage.js";
+import { renderMenuPage } from "./menuPage.js";
 
 const content = document.querySelector('#content');
 const header = document.createElement('header');
 const headerTab = ["Home", "Menu", "Contact"];
 
 for (let i = 0; i < headerTab.length; i++) {
-    const div = document.createElement('div');
-    div.textContent = headerTab[i];
-    header.appendChild(div);
+    const btn = document.createElement('button');
+    btn.classList.add(headerTab[i]);
+    btn.textContent = headerTab[i];
+    header.appendChild(btn);
 }
 
 content.appendChild(header);
 
 document.body.appendChild(renderHomePage());
+
+const menuTab = document.querySelector('.Menu');
+console.log(menuTab);
+
+menuTab.addEventListener('click', () => {
+    content.removeChild(content.lastChild);
+    document.body.appendChild(renderMenuPage());
+})
