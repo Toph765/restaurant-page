6b113ac51e6fb576839a70922d8f503c39c70884
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-11-15 11:45:25 +0800

    Refactor index.js and main.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,15 @@
-function component() {
-    const content = document.querySelector('#content');
-    const header = document.createElement('header');
-    const main = document.createElement('main');
+import { renderHomePage } from "./homePage.js";
 
-    const headerTab = ["Home", "Menu", "Contact"];
-    const mainContent = ["image", "name", "text"]
+const content = document.querySelector('#content');
+const header = document.createElement('header');
+const headerTab = ["Home", "Menu", "Contact"];
 
-    for (let i = 0; i < headerTab.length; i++) {
-        const div = document.createElement('div');
-        div.textContent = headerTab[i];
-        header.appendChild(div);
-    }
-
-    for (let j = 0; j < mainContent.length ; j++) {
-        if (mainContent[j] === 'image') {
-            const image = document.createElement('img');
-            image.setAttribute('src', 'food-pexels-dorota-semla.jpg');
-            image.setAttribute('alt', 'food');
-            main.appendChild(image);
-        } else if (mainContent[j] === 'name') {
-            const name = document.createElement('h1');
-            name.textContent = 're: Baratie';
-            main.appendChild(name);
-        } else if (mainContent[j] === 'text') {
-            const description = document.createElement('div');
-            description.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt repellat incidunt ducimus facilis, accusantium aperiam reprehenderit, nam quia, molestiae perferendis in possimus optio rerum maxime? Dolorem culpa ipsum ullam!'
-            main.appendChild(description);
-        }
-    }
-
-    content.appendChild(header);
-    content.appendChild(main);
-
-    return content;
+for (let i = 0; i < headerTab.length; i++) {
+    const div = document.createElement('div');
+    div.textContent = headerTab[i];
+    header.appendChild(div);
 }
 
+content.appendChild(header);
 
-document.body.appendChild(component());
+document.body.appendChild(renderHomePage());
