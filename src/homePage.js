6742097d250b533466fd91bc487c7f3c64d766c7
Christopher Cerda <121602675+Toph765@users.noreply.github.com>
2023-11-30 13:17:44 +0800

    Add style to homepage

--- src/homePage.js
+++ src/homePage.js
@@ -1,25 +1,29 @@
 
 function renderHomePage() {
     const main = document.createElement('main');
-    const mainContent = ["image", "name", "text", "schedule"];
+    const mainContent = ["name", "image", "text", "schedule"];
 
     for (let j = 0; j < mainContent.length ; j++) {
         if (mainContent[j] === 'image') {
             const image = document.createElement('img');
+            image.classList.add('homeImg');
             image.setAttribute('src', 'food-pexels-dorota-semla.jpg');
             image.setAttribute('alt', 'food');
             main.appendChild(image);
         } else if (mainContent[j] === 'name') {
             const name = document.createElement('h1');
+            name.classList.add('homeName');
             name.textContent = 're: Baratie';
             main.appendChild(name);
         } else if (mainContent[j] === 'text') {
             const description = document.createElement('div');
+            description.classList.add('homeText');
             description.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius deserunt repellat incidunt ducimus facilis, accusantium aperiam reprehenderit, nam quia, molestiae perferendis in possimus optio rerum maxime? Dolorem culpa ipsum ullam!'
             main.appendChild(description);
         } else if (mainContent[j] === 'schedule') {
             const schedule = document.createElement('div');
-            schedule.textContent = 'Tuesday - Friday: 10:00am - 8:00pm Saturday & Sunday: 10am - 12:00pm';
+            schedule.classList.add('homeSchedule');
+            schedule.textContent = 'Tuesday - Friday: 10:00am - 8:00pm || Saturday & Sunday: 10am - 12:00pm';
             main.appendChild(schedule);
         }
     }
